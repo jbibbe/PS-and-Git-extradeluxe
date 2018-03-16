b5140d7d300945e37959052ec452e10a4fcf2736
Jeroen Bibbe <jeroen.bibbe@coolblue.nl>
2018-03-16 13:52:07 +0100

    added Microsoft.PowerShell_profile.ps1

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,70 @@
+
+Start-SshAgent
+
+Import-Module Get-ChildItemColor
+
+Import-Module posh-git
+
+Import-Module PSReadLine
+
+Set-Alias l Get-ChildItemColor -option AllScope
+Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+
+
+$global:GitPromptSettings.BeforeText = '['
+$global:GitPromptSettings.AfterText  = '] '
+
+$GitPromptSettings.DefaultPromptSuffix = '`n$(''>'' * ($nestedPromptLevel + 1)) '
+$GitPromptSettings.DefaultPromptPrefix = '[$(hostname)] '
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+
+# http://serverfault.com/questions/95431
+function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function prompt {
+    # https://github.com/dahlbyk/posh-git/wiki/Customizing-Your-PowerShell-Prompt
+    $origLastExitCode = $LastExitCode
+    Write-VcsStatus
+
+    if (Test-Administrator) {  # if elevated
+        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+    }
+
+    Write-Host "$env:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+    Write-Host "$env:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $curPath = $ExecutionContext.SessionState.Path.CurrentLocation.Path
+    if ($curPath.ToLower().StartsWith($Home.ToLower()))
+    {
+        $curPath = "~" + $curPath.SubString($Home.Length)
+    }
+
+    Write-Host $curPath -NoNewline -ForegroundColor Blue
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    $LastExitCode = $origLastExitCode
+    "`n$('>' * ($nestedPromptLevel + 1)) "
+}
+
+
+
+Set-PSReadLineOption -HistoryNoDuplicates
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -HistorySaveStyle SaveIncrementally
+Set-PSReadLineOption -MaximumHistoryCount 4000
+
+# history substring search
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Tab completion
+Set-PSReadlineKeyHandler -Chord 'Shift+Tab' -Function Complete
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+
+
